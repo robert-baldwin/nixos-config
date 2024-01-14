1d0834487bd2cc32f60cb9e50cfde4f1b2a519b8
Robert Baldwin <rob.baldwin@hey.com>
2024-01-14 15:39:09 -0500

    Configure starship

--- programs/starship.nix
+++ programs/starship.nix
@@ -2,5 +2,10 @@
   programs.starship = {
     enable = true;
     enableFishIntegration = true;
+    settings = {
+      add_newline = false;
+      line_break.disabled = true;
+      package.disabled = true;
+    };
   };
 }
