3bfb5b05bd211b75a8b24d95ddcdd9b0dd157a1c
Robert Baldwin <rob.baldwin@hey.com>
2024-01-21 22:34:32 -0500

    Move shared host configuration from desktop/default.nix to configuration.nix

--- hosts/configuration.nix
+++ hosts/configuration.nix
@@ -1,3 +1,5 @@
+{ pkgs, inputs, ... }:
+
 {
   # Configure Bootloader.
   boot.loader.systemd-boot.enable = true;
@@ -35,6 +37,69 @@
 
   # Enable git
   programs.git.enable = true;
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Enable sound with pipewire
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    wireplumber.enable = true;
+  };
+
+  # List packages installed in system profile
+  environment.systemPackages = with pkgs; [
+    curl
+    wget
+  ];
+
+  # Install fonts
+  fonts.fontDir.enable = true;
+  fonts.packages = with pkgs; [
+    nerdfonts
+    google-fonts
+    font-awesome
+  ];
+
+  # Configure login manager
+  services.xserver = {
+    displayManager.gdm.enable = true;
+    layout = "us";
+    xkbVariant = "";
+  };
+
+  # Enable bluetooth with blueman
+  services.blueman.enable = true;
+  hardware.bluetooth.enable = true;
+
+  # Install Docker virtualization
+  virtualisation.docker.enable = true;
+
+  # Enable hyprland compositor
+  xdg.portal = {
+    enable = true;
+  };
+
+  programs.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+    package = inputs.hyprland.packages.${pkgs.system}.hyprland;
+    portalPackage = inputs.xdg-desktop-portal-hyprland.packages."${pkgs.system}".xdg-desktop-portal-hyprland;
+  };
+
+  # Configure user
+  users.users.sprout = {
+    shell = pkgs.fish;
+    extraGroups = [ "networkmanager" "wheel" "video" ];
+  };
+  programs.fish.enable = true;
+
 
   # Flake support
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -1,28 +1,7 @@
-{ inputs, config, pkgs, ...}:
+{ config, pkgs, ...}:
 {
   imports = [
     ./hardware-configuration.nix
-  ];
-
-  # Enable networking
-  networking.networkmanager.enable = true;
-
-  # Enable sound with pipewire
-  sound.enable = true;
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    wireplumber.enable = true;
-  };
-
-  # List packages installed in system profile
-  environment.systemPackages = with pkgs; [
-    curl
-    wget
   ];
 
   environment.sessionVariables = {
@@ -32,7 +11,6 @@
     __GLX_VENDOR_LIBRARY_NAME = "nvidia";
     LIBVA_DRIVER_NAME = "nvidia";
   };
-
 
   # OpenGL support
   hardware.opengl = {
@@ -56,45 +34,4 @@
     nvidiaSettings = true;
     package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
-
-  # Install fonts
-  fonts.fontDir.enable = true;
-  fonts.packages = with pkgs; [
-    nerdfonts
-    google-fonts
-    font-awesome
-  ];
-
-  # Configure login manager
-  services.xserver = {
-    displayManager.gdm.enable = true;
-    layout = "us";
-    xkbVariant = "";
-  };
-
-  # Enable bluetooth with blueman
-  services.blueman.enable = true;
-  hardware.bluetooth.enable = true;
-
-  # Install Docker virtualization
-  virtualisation.docker.enable = true;
-
-  # Enable hyprland compositor
-  xdg.portal = {
-    enable = true;
-  };
-
-  programs.hyprland = {
-    enable = true;
-    xwayland.enable = true;
-    package = inputs.hyprland.packages.${pkgs.system}.hyprland;
-    portalPackage = inputs.xdg-desktop-portal-hyprland.packages."${pkgs.system}".xdg-desktop-portal-hyprland;
-  };
-
-  # Configure user
-  users.users.sprout = {
-    shell = pkgs.fish;
-    extraGroups = [ "networkmanager" "wheel" "video" ];
-  };
-  programs.fish.enable = true;
 }
